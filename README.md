1324f4e277fc747a623578fc217bf9542c87d4e5
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-16 23:01:39 -0700

    Refactor max pairwise product

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Algorithmic Toolbox
+# Algorithms in Python
+Programming challenges and algorithm implementations in Python 3.
+
+## Table of Contents
+* [Objective](#objective)
+* [Programming Challenges](#programming-challenges)
+  - [Maximum Pairwise Product](#maximum-pairwise-product)
+* [Greedy Algorithms](#greedy-algorithms)
+* [Divide and Conquer](#divide-and-conquer)
+* [Dynamic Programming](#dynamic-programming)
+
+## Objective
+Practice implementing various algorithms in Python and gain experience designing algorithms.
+
+## Programming Challenges
+
+### Maximum Pairwise Product
+Find the maximum product of two distinct numbers in a sequence of non-negative integers.  
+**Input:** A sequence of non-negative integers.  
+**Output:** The maximum value that can be obtained by multiplying two different elements from the sequence.  
+[Python Code](max_pairwise_product.py "max_pairwise_product.py")
+
+## Greedy Algorithms
+
+## Divide and Conquer
+
+## Dynamic Programming
+
+## Technologies
+Python 3.8
--- max_pairwise_product.py
+++ max_pairwise_product.py
@@ -1,27 +1,18 @@
-# python3
+# Maximum Pairwise Product
+# Author: jerrybelmonte
+# Input: First line contains an integer n (2 <= n <= 2*10E5). Second line
+# contains n non-negative integers (0 <= a1,...,an <= 2*10E5).
+# Output: Integer of the maximum pairwise product.
 
 
-def max_pairwise_product(numbers):
-    n = len(numbers)
-    max1 = max(numbers)
-    first_ndx = 0
-    for ndx in range(n):
-        if numbers[ndx] == max1:
-            first_ndx = ndx
-            break
-    max2 = 0
-    if first_ndx != 0:
-        max2 = numbers[0]
-    else:
-        max2 = numbers[1]
-    for ndx in range(n):
-        if ndx != first_ndx:
-            max2 = max(max2, numbers[ndx])
-    max_product = max1 * max2
-    return max_product
+def maximum_pairwise_product(numbers):
+    # sorting in descending order is O(n*logn)
+    numbers.sort(reverse=True)
+    # the first two numbers will produce the largest product
+    return numbers[0] * numbers[1]
 
 
 if __name__ == '__main__':
     input_n = int(input())
-    input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+    input_numbers = [int(num) for num in input().split()]
+    print(maximum_pairwise_product(input_numbers))
