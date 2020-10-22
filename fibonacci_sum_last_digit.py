a66dfe3eccd70acba3fd641e3bc78a13ba18fddd
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-22 00:08:29 -0700

    Refactor fibonacci sum last digit

--- fibonacci_sum_last_digit.py
+++ fibonacci_sum_last_digit.py
@@ -1,33 +1,37 @@
-# Uses python3
-def fibonacci_sum_naive(n):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current = 1
-    fsum = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        fsum += current
-
-    return fsum % 10
+# Last Digit of the Sum of Fibonacci Numbers
+# Author: jerrybelmonte
+# Input: Single integer n.
+# Output: The last digit of the sum of F0 + F1 +...+ Fn.
 
 
-def fibonacci_sum_last_digit(n):
-    num = n % 60
+def fibonacci_sum_last_digit(n: int):
+    """
+    Finds the last digit of a sum of the first n Fibonacci numbers.
 
-    if num < 2:
-        return num
+    :param n: the number of Fibonacci numbers
+    :return: the last digit of the sum
 
-    fib = [0, 1]
+    Example 1: F0 + F1 + F3 = 0 + 1 + 1 + 2 = 4
+    >>>fibonacci_sum_last_digit(3)
+    >>>4
 
-    for i in range(2, num + 1):
-        fib.append(((fib[i - 1] + fib[i - 2]) % 10))
+    Example 2: Sum = 927,372,692,193,078,999,175
+    >>>fibonacci_sum_last_digit(100)
+    >>>5
+    """
+    digit = n % 60  # the Pisano period for the fibonacci sum is 60
 
-    return sum(fib) % 10
+    if digit < 2:
+        return digit
+
+    fibonacci = [0, 1]  # holds the last digit of the fibonacci sum
+
+    for i in range(2, digit + 1):
+        last_digit = (fibonacci[i - 1] + fibonacci[i - 2]) % 10
+        fibonacci.append(last_digit)
+
+    return sum(fibonacci) % 10
 
 
 if __name__ == '__main__':
-    n_ = int(input())
-    print(fibonacci_sum_last_digit(n_))
+    print(fibonacci_sum_last_digit(int(input())))
