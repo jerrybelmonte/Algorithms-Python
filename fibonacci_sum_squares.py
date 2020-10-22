b9f7cee4a77e6a16f798e76fa9b5732a616af6a2
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-22 01:00:24 -0700

    Refactor fibonacci sum of squares

--- fibonacci_sum_squares.py
+++ fibonacci_sum_squares.py
@@ -1,39 +1,44 @@
-# Uses python3
-def fibonacci_sum_squares_naive(n):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current = 1
-    sum_sq = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum_sq += current * current
-
-    return sum_sq % 10
+# Last Digit of the Sum of Squares of Fibonacci Numbers
+# Author: jerrybelmonte
+# Input: Single integer n.
+# Output: The last digit of F0^2 + F1^2 +...+ Fn^2.
 
 
-def fibonacci_sum_squares(n):
+def fibonacci_sum_squares(n: int):
+    """
+    Computes the last digit of F0^2 + F1^2 + ... + Fn^2.
+
+    :param n: the sequence size of Fibonacci numbers
+    :return: the last digit of the sum of squares
+
+    Example 1:
+    0+1+1+4+9+25+64+169=273
+    >>>fibonacci_sum_squares(7)
+    >>>3
+
+    Example 2:
+    F73 = 1,052,478,208,141,359,608,061,842,155,201
+    >>>fibonacci_sum_squares(73)
+    >>>1
+    """
     n %= 60
 
     if n < 2:
         return n
 
-    fib = [0, 1]
-    fsum = 0
+    fibonacci = [0, 1]
+    fibonacci_sq_sum = 0
 
     for i in range(2, n + 1):
-        num = (fib[i - 1] + fib[i - 2]) % 10
-        fib.append(num)
+        last_digit = (fibonacci[i - 1] + fibonacci[i - 2]) % 10
+        fibonacci.append(last_digit)
 
-    for num in fib:
-        fsum += (num * num) % 10
-        fsum %= 10
+    for num in fibonacci:
+        fibonacci_sq_sum += num**2 % 10
+        fibonacci_sq_sum %= 10
 
-    return fsum
+    return fibonacci_sq_sum
 
 
 if __name__ == '__main__':
-    n_ = input()
-    print(fibonacci_sum_squares(int(n_)))
+    print(fibonacci_sum_squares(int(input())))
