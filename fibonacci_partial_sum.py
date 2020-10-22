c312afcb9ca80025b43fc212b516c19f7965eb3f
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-22 01:00:04 -0700

    Refactor fibonacci partial sum

--- fibonacci_partial_sum.py
+++ fibonacci_partial_sum.py
@@ -1,37 +1,50 @@
-# Uses python3
-def fibonacci_partial_sum_naive(from_i, to_i):
-    psum = 0
-    current = 0
-    next_s = 1
-
-    for i in range(to_i + 1):
-        if i >= from_i:
-            psum += current
-
-        current, next_s = next_s, current + next_s
-
-    return psum % 10
+# Last Digit of the Partial Sum of Fibonacci Numbers
+# Author: jerrybelmonte
+# Input: Two non-negative integers m and n separated by a space.
+# Output: The last digit of Fm + Fm+1 +...+ Fn.
 
 
-def fibonacci_partial_sum(m, n):
-    fib = [0, 1]
-    if m == n:
-        num = n % 60
-        if num <= 1:
-            return num
-        for i in range(2, num + 1):
-            fib.append(((fib[i - 1] + fib[i - 2]) % 10))
-        return fib[num]
-    start_ndx = m % 60
-    end_ndx = n % 60 + 1
+def fibonacci_partial_sum(m: int, n: int):
+    """
+    Finds the lsat digit of a partial sum of Fibonacci numbers:
+    Fm + Fm+1 + ... + Fn.
+
+    :param m: starting index in Finacci sequence
+    :param n: end index in Fibonacci sequence
+    :return: the last digit of the partial sum
+
+    Example: F3 + F4 + F5 + F7 = 2 + 3 + 5 + 8 + 13 = 31
+    >>>fibonacci_partial_sum(3, 7)
+    >>>1
+    """
+    pisano_period = 60
+    partial_sum = [0, 1]
+
+    if m == n:  # base case for a single fibonacci number
+        fibonacci = n % pisano_period
+
+        if fibonacci <= 1:  # base case for Fn <= 1
+            return fibonacci
+
+        for i in range(2, fibonacci + 1):  # compute the fibonacci sequence
+            digit = (partial_sum[i - 1] + partial_sum[i - 2]) % 10
+            partial_sum.append(digit)
+
+        return partial_sum[fibonacci]
+
+    start_ndx = m % pisano_period
+    end_ndx = n % pisano_period + 1
+
     if start_ndx >= end_ndx:
-        end_ndx += 60
+        end_ndx += pisano_period
+
     for i in range(2, end_ndx):
-        fib.append(((fib[i - 1] + fib[i - 2]) % 10))
-    return sum(fib[start_ndx:end_ndx]) % 10
+        digit = (partial_sum[i - 1] + partial_sum[i - 2]) % 10
+        partial_sum.append(digit)
+
+    return sum(partial_sum[start_ndx:end_ndx]) % 10
 
 
 if __name__ == '__main__':
-    from_, to_ = input().split()
-    # print('Naive:', fibonacci_partial_sum_naive(int(from_), int(to_)))
-    print(fibonacci_partial_sum(int(from_), int(to_)))
+    start, end = input().split()
+    print(fibonacci_partial_sum(int(start), int(end)))
