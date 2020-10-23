6599b0dc1b9d22231b7313994e01ff16793b4c32
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-23 01:23:06 -0700

    Add doctest for dot product

--- dot_product.py
+++ dot_product.py
@@ -1,19 +1,27 @@
-# Uses python3
+# Maximum Advertisement Revenue (Maximum Dot Product)
+# Author: jerrybelmonte
+# Input: First line is an integer n, second line contains sequence a,
+# and the third line contains the sequence b.
+# Output: The maximum possible value of the two sequences.
 import sys
 
 
-def max_dot_product(a, b):
-    a = sorted(a)
-    b = sorted(b)
-    res = 0
-    for i in range(len(a)):
-        res += a[i] * b[i]
-    return res
+def max_dot_product(a: list, b: list):
+    """
+    >>> max_dot_product([23], [39])
+    897
+
+    >>> max_dot_product([1, 3, -5], [-2, 4, 1])
+    23
+    """
+    a.sort()
+    b.sort()
+
+    return sum([a[i]*b[i] for i in range(len(a))])
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    data = list(map(int, sys.stdin.read().split()))
     n = data[0]
     a = data[1:(n + 1)]
     b = data[(n + 1):]
