c48372e30af6cbb22245248524f75cf8dcce9c42
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-23 01:38:14 -0700

    Add doctest for car fueling

--- car_fueling.py
+++ car_fueling.py
@@ -1,24 +1,48 @@
-# python3
+# Car fueling
+# Author: jerrybelmonte
+# Input: The distance d, the miles per tank m, the number of stops n,
+# and the distances of each gas station from the starting point.
+# Output: The minimum number of pitstops.
 import sys
 
 
-def compute_min_refills(distance, tank, stops):
-    n = len(stops)
-    prev, cur = 0, 0
-    d = [0] + stops + [distance]
+def compute_min_refills(distance: int, tank: int, stops: list):
+    """
+    Computes the minimum number of gas station pit stops.
+
+    >>> compute_min_refills(950, 400, [200, 375, 550, 750])
+    2
+
+    >>> compute_min_refills(10, 3, [1, 2, 5, 9])
+    -1
+
+    Example 3:
+    >>> compute_min_refills(200, 250, [100, 150])
+    0
+    """
+    previous, current = 0, 0
+    positions = [0] + stops + [distance]
+
     num_refills, cur_position = 0, 0
-    while cur <= n:
-        prev = cur
-        while cur <= n and (d[cur+1] - d[prev]) <= tank:
-            cur += 1
-            cur_position = d[cur]
-        if cur == prev:
-            return -1
+
+    while current <= len(stops):
+        previous = current
+
+        while current <= len(stops) and (
+                positions[current + 1] - positions[previous]
+        ) <= tank:
+            current += 1
+            cur_position = positions[current]
+
+        if current == previous:
+            return -1  # destination not possible
+
         if cur_position < distance:
             num_refills += 1
+
     return num_refills
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     d, m, _, *stops = map(int, sys.stdin.read().split())
     print(compute_min_refills(d, m, stops))
