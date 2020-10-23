2035c393c7ba2044a924e100eb0077e5f3d173f5
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-23 03:49:33 -0700

    Add doctest for majority element

--- majority_element.py
+++ majority_element.py
@@ -1,32 +1,54 @@
-# Uses python3
+# Majority Element
+# Author: jerrybelmonte
 import sys
 
 
-def get_majority_element(a, left, right):
+def get_majority_element(seq, left, right):
+    """
+    Gets the element in the sequence that makes up the majority if it exists.
+
+    :param seq: sequence of elements
+    :param left: starting index in the sequence
+    :param right: end index in the sequence
+    :return: the majority element count, otherwise -1 if DNE
+
+    >>> get_majority_element([2, 3, 9, 2, 2], 0, 5)
+    3
+
+    >>> get_majority_element([1, 2, 3, 4], 0, 4)
+    -1
+
+    >>> get_majority_element([1, 2, 3, 1], 0, 4)
+    -1
+    """
     if left == right:
         return -1
+
     if left + 1 == right:
-        return a[left]
-    # write your code here
-    a = sorted(a)
-    # print(a)
+        return seq[left]
+
+    seq.sort()
     cur = left
     ndx = left
+
     while ndx < right:
         count = 0
-        while ndx < right and a[ndx] == a[cur]:
+
+        while ndx < right and seq[ndx] == seq[cur]:
             ndx += 1
             count += 1
+
         if count > left + ((right - left)//2):
             return ndx
+
         cur = ndx
+
     return -1
 
 
 if __name__ == '__main__':
-    input_ = sys.stdin.read()
-    n_, *a_ = list(map(int, input_.split()))
-    if get_majority_element(a_, 0, n_) != -1:
+    n, *a = list(map(int, sys.stdin.read().split()))
+    if get_majority_element(a, 0, n) != -1:
         print(1)
     else:
         print(0)
