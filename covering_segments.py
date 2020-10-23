c856cb1754c6dec92f6498ded1b6451185dd7cb8
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-23 01:40:52 -0700

    Add doctest for covering segments

--- covering_segments.py
+++ covering_segments.py
@@ -1,4 +1,9 @@
-# Uses python3
+# Collecting Signatures (Covering Segments by Points)
+# Author: jerrybelmonte
+# Input: First line contains the number of segments n.
+# The following lines contains two integers ai and bi for the segment i.
+# Output: The minimum number m of points in the first line.
+# Followed by the integer coordinates of m points separated by a space.
 import sys
 from collections import namedtuple
 
@@ -6,27 +11,39 @@ Segment = namedtuple('Segment', 'start end')
 
 
 def optimal_points(segments):
-    p = [(s.start, s.end) for s in segments]
-    p = sorted(p, key=lambda s: s[0])
-    cur = p[0]
-    end = cur[1]
+    """
+    Example 1:
+    >>> optimal_points([(1, 3), (2, 5), (3, 6)])
+    [3]
+
+    Example 2:
+    >>> optimal_points([(4, 7), (1, 3), (2, 5), (5, 6)])
+    [3, 6]
+    """
+    segments.sort(key=lambda s: s[0])
+    end = segments[0][1]
     point = []
-    for i in range(1, len(p)):
-        start, stop = p[i][0], p[i][1]
+
+    for i in range(1, len(segments)):
+        start, stop = segments[i]
+
         if end < start:
             point.append(end)
             end = stop
         elif stop < end:
             end = stop
+
     if end not in point:
         point.append(end)
+
     return point
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *data = map(int, input.split())
-    segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
+    segments = list(map(lambda x: Segment(
+        x[0], x[1]), zip(data[::2], data[1::2])))
     points = optimal_points(segments)
     print(len(points))
     print(*points)
