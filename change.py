c12cc18e6733a1e7e27ccc9afba5fcd50a0a5d52
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-22 23:02:37 -0700

    Refactor change

--- change.py
+++ change.py
@@ -1,18 +1,32 @@
-# Uses python3
-def get_change(m):
+# Money Change
+# Author: jerrybelmonte
+# Input: Integer m of the money given.
+# Output: The minimum number of coins with denominations 1, 5, and 10.
+
+
+def get_change(money: int):
+    """
+    Greggy implementation used by cashiers for getting change.
+
+    :param money: the amount we need change for
+    :return: the minimum number of coins
+    """
     min_coins = 0
-    while m >= 10:
+
+    while money >= 10:
         min_coins += 1
-        m -= 10
-    while m >= 5:
+        money -= 10
+
+    while money >= 5:
         min_coins += 1
-        m -= 5
-    while m > 0:
+        money -= 5
+
+    while money > 0:
         min_coins += 1
-        m -= 1
+        money -= 1
+
     return min_coins
 
 
 if __name__ == '__main__':
-    m_ = int(input())
-    print(get_change(m_))
+    print(get_change(int(input())))
