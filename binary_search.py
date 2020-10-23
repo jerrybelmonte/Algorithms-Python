b3ba202bd63d785f0b57305210c03c51b7745a02
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-10-23 03:42:06 -0700

    Add binary search doctest

--- binary_search.py
+++ binary_search.py
@@ -1,34 +1,47 @@
-# Uses python3
+# Binary Search
+# Author: jerrybelmonte
 import sys
 
 
-def binary_search(a, x):
-    left, right = 0, len(a) - 1
+def binary_search(num_seq: list, key_val: int):
+    """
+    Binary search algorithm implementation.
+
+    :param num_seq: sorted sequence of n pairwise distinct positive integers
+    :param key_val: key value to search for in the sequence of numbers
+    :return: the index of the element within the sequence of numbers
+
+    >>> binary_search([1, 5, 8, 12, 13], 8)
+    2
+
+    >>> binary_search([1, 5, 8, 12, 13], 23)
+    -1
+
+    >>> binary_search([1, 5, 8, 12, 13], 1)
+    0
+
+    >>> binary_search([1, 5, 8, 12, 13], 11)
+    -1
+    """
+    left, right = 0, len(num_seq) - 1
 
     while left <= right:
-        mid = left + ((right - left) // 2)
-        if a[mid] == x:
+        mid = left + (right - left)//2
+
+        if num_seq[mid] == key_val:
             return mid
-        elif x < a[mid]:
+        elif key_val < num_seq[mid]:
             right = mid - 1
         else:
             left = mid + 1
-    return -1
 
-
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
     return -1
 
 
 if __name__ == '__main__':
-    input_ = sys.stdin.read()
-    data = list(map(int, input_.split()))
+    data = list(map(int, sys.stdin.read().split()))
     n = data[0]
     m = data[n + 1]
-    a1 = data[1:n + 1]
+    a = data[1:n + 1]
     for num in data[n + 2:]:
-        # replace with the call to binary_search when implemented
-        print(binary_search(a1, num), end=' ')
+        print(binary_search(a, num), end=' ')
